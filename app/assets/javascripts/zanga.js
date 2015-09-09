2936f580cb44bcbce371b5ab94bd232a25ec6a1e
TylerBANDArielH <TylerBANDArielH@aa.io>
2015-09-09 10:35:23 -0400

    Initial commit. Setup project

---
+++ app/assets/javascripts/zanga.js
@@ -0,0 +1,13 @@
+window.Zanga = {
+  Models: {},
+  Collections: {},
+  Views: {},
+  Routers: {},
+  initialize: function() {
+    alert('Hello from Backbone!');
+  }
+};
+
+$(document).ready(function(){
+  Zanga.initialize();
+});
